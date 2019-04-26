665da4aa8e8965ca0f6b495f3fa57c045c00f173
tiaires <48353968+tiaires@users.noreply.github.com>
2019-04-26 17:17:44 +0100

    Initial commit
    Depois De algum Trabalho 3 tabelas criadas

---
+++ app/src/main/java/pt/ipg/myapplication/MainActivity.java
@@ -0,0 +1,66 @@
+package pt.ipg.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button ed_enq,abrir,cancelar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            abrir = findViewById(R.id.button);
+            ed_enq = findViewById(R.id.button3);
+            cancelar=findViewById(R.id.button2);
+
+
+            abrir.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    abrirInquerito();
+                }
+            });
+
+
+            ed_enq.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    abrirEdit();
+                }
+            });
+
+            cancelar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    cancelar();
+                }
+            });
+
+    }
+
+        public void abrirInquerito(){
+
+                Intent intent = new Intent(this, inquerito.class);
+                startActivity(intent);
+        }
+
+        public void abrirEdit(){
+
+                Intent intent = new Intent(this, ed_inqueiro.class);
+                startActivity(intent);
+        }
+
+        public void cancelar(){
+
+            finish();
+        }
+
+}
---
+++ app/src/main/java/pt/ipg/myapplication/ed_inqueiro.java
@@ -0,0 +1,13 @@
+package pt.ipg.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ed_inqueiro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ed_inqueiro);
+    }
+}
